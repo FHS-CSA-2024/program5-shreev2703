817cd1e795abdb13f64269eecd16cb39b1d74afb
shreev2703 <67807046+shreev2703@users.noreply.github.com>
2024-08-19 19:47:59 -0700

    ShreevatsonBabuSreegeethaProgram5.java

--- Program5.java
+++ Program5.java
@@ -1,9 +1,24 @@
-//import stuff here?
-
-//Your code here
-
-//Paste console output below:
-/*
-
-
-*/
+public class AverageMilesPerGallon {
+    public static void main(String[] args) {
+        // Data of the vehicles
+        int royaleMiles = 286;
+        int royaleGallons = 9;
+        int koopaKingMiles = 412;
+        int koopaKingGallons = 40;
+        int pipeFrameMiles = 361;
+        int pipeFrameGallons = 18;
+        int badwagonMiles = 161;
+        int badwagonGallons = 11;
+        // Averages calculating
+        double royaleAvg = (double) royaleMiles / royaleGallons;
+        double koopaKingAvg = (double) koopaKingMiles / koopaKingGallons;
+        double pipeFrameAvg = (double) pipeFrameMiles / pipeFrameGallons;
+        double badwagonAvg = (double) badwagonMiles / badwagonGallons;
+        //Print statements
+        System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon:");
+        System.out.println("Royale averaged " + Math.round(royaleAvg * 10.0) / 10.0 + " m/g");
+        System.out.println("Koopa King averaged " + Math.round(koopaKingAvg * 10.0) / 10.0 + " m/g");
+        System.out.println("Pipe Frame averaged " + Math.round(pipeFrameAvg * 10.0) / 10.0 + " m/g");
+        System.out.println("Badwagon averaged " + Math.round(badwagonAvg * 10.0) / 10.0 + " m/g");
+    }
+}
